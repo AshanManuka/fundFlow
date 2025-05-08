ac7edaad07661429550791e925d7b0f69f9a9d77
AshanManuka <manukajayarathne.coma@gmail.com>
2025-05-08 20:19:46 +0530

    configure app basic, pom and db

---
+++ src/main/java/com/fundflow/fundFlowApp/FundFlowAppApplication.java
@@ -0,0 +1,13 @@
+package com.fundflow.fundFlowApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundFlowAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FundFlowAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fundflow/fundFlowApp/FundFlowAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fundflow.fundFlowApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FundFlowAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
